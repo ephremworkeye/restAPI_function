85afd090edc7141c77ce708057ba41ad2597c9f3
Ephrem Wube <ephremworkeye@gmail.com>
2021-07-31 03:59:06 -0700

    model added and registered

--- function_rest/models.py
+++ function_rest/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+
+class Movie(models.Model):
+    category = models.ForeignKey(Category, related_name='movies', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    max_length = models.PositiveIntegerField()
+    revenue = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    
+
